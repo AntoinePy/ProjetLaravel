f199df6ddcfafed07a0ebf99be3ba6250dc61d75
antoine.py@univ-fcomte.fr <guildantoine@hotmail.fr>
2018-01-05 20:18:38 +0100

    Suite charts

--- app/Http/Controllers/StatistiquesController.php
+++ app/Http/Controllers/StatistiquesController.php
@@ -13,12 +13,16 @@ class StatistiquesController extends Controller
 
     public function create(){
         if(Auth::check()){
-            $chart = Charts::create('line','highcharts')
+            $chart = Charts::create('bar','morris')
                 ->Title('Exemple')
                 ->Labels(['First','Second','Third'])
                 ->Values([5,10,20])
+                ->ElementLabel("Total KW")
+                ->Width(0)
+                ->Height(400)
+                ->Colors(["#ff0000"])
                 ->Dimensions(1000,500)
-                ->Responsive(false);
+                ->Responsive(true);
             return view('statistiques',['chart' => $chart]);
         }else{
             return view('auth\login');
