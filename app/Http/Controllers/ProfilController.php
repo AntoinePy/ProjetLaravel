24dd7a2af7f66658d3a742d208fa837ed8998329
antoine.py@univ-fcomte.fr <guildantoine@hotmail.fr>
2018-01-08 21:38:56 +0100

    Debut form profil ( fonctionne pas )

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\User;
+
 
 
 class ProfilController extends Controller
 {
     public function create(){
         if(Auth::check()){
+            $user = User::all();
             return view('profil');
         }else{
             return view('auth\login');
--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
 @section('content')
 
-Profil
+<h1>Profil</h1>
+    {!! Form::open(['method'=>'put']) !!}
+    <div class="form-group">
+        {!! Form::label('name','Name') !!}
+        {!! Form::text('name',$user->name,['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('email','E-mail') !!}
+        {!! Form::text('email',$user->email,['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('password','Password') !!}
+        {!! Form::text('password',$user->password,['class'=>'form-control']) !!}
+    </div>
+    <button class="btn btn-primary">Valider</button>
+    {!! Form::close() !!}
 @endsection
