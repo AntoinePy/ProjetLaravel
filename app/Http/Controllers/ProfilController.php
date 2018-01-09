2006b0d95746e32238b3dfebc044ffaa1f6f51f9
antoine.py@univ-fcomte.fr <guildantoine@hotmail.fr>
2018-01-09 22:20:47 +0100

    Profil

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\User;
+use Illuminate\Support\Facades\DB;
 
 
 
@@ -12,8 +13,10 @@ class ProfilController extends Controller
 {
     public function create(){
         if(Auth::check()){
-            $user = User::all();
-            return view('profil');
+            $user = DB::table('users')->first();
+            echo $user->name;
+            echo $user->email;
+            return view('profil',['user'=>$user]);
         }else{
             return view('auth\login');
         }
--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -3,6 +3,10 @@
 
 <h1>Profil</h1>
             Afficher les infos de l'user ici
+{{--
+    {!! $user->render() !!}
+--}}
+
 <a href="{{url('/profil/edit')}}">
     Modifier vos infos
 </a>
